6be8b950bee40c8fe4d233d9e7a3e9f45c4d4e97
k1ssa1 <samikissai.edu@gmail.com>
2024-07-19 23:42:34 +0200

    feat: add basic router

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider , Outlet} from 'react-router-dom'
+
+const Layout = () => {
+  <div>
+    <Outlet/>
+  </div>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <App/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <RouterProvider router={router}/> 
 )
