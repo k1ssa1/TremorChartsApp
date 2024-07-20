f242119e2bee066ad18860f40a14e6d4c48f3b8e
k1ssa1 <samikissai.edu@gmail.com>
2024-07-20 21:53:43 +0200

    feat: add basic magnitude/number bar chart feature

---
+++ src/components/basic/barcharts/MagnitudeChart.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
+
+const MgnitudeChart = ({data}) => {
+
+    const [chartData, setChartData] = useState({labels: [], values: []})
+
+    useEffect(() => {
+        setChartData({
+            labels: ['<= 1', ']1-2]', ']2-3]', ']3-4]', ']4-5]', '>5'],
+            values: data
+        })
+    }, [data])
+
+    return ( 
+        <Plot
+            data={[
+                {
+                    type: 'bar',
+                    x: chartData.labels,
+                    y: chartData.values,
+                    marker: {color: 'black'}
+                }
+            ]}
+            layout={{
+                width: 700,
+                height: 450,
+                title: "Number of earthquakes by Magnitude rane from 0 to 6+",
+                xaxis:{
+                    title: 'Magnitude range'
+                },
+                yaxis:{
+                    title: 'Number of earthquakes'
+                }
+            }}
+        />
+    );
+}
+ 
+export default MgnitudeChart;
--- src/views/basic/BarChart.jsx
+++ src/views/basic/BarChart.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+
+import { instance } from "../../config/axios.instance.";
+
+import MagnitudeChart from "../../components/basic/barcharts/MagnitudeChart";
+
 const BarChart = () => {
+
+    const [data, setData] = useState([])
+
+    async function getData(){
+        try{
+            const req = await instance.get("query?format=geojson&starttime=2023-09-07&endtime=2023-09-15");
+            const res = req.data;
+            const magnitueArray = []
+            res.features.forEach(item => {
+                const mag = item.properties.mag;
+                magnitueArray.push(mag)
+            })
+            const sortedArray = magnitueArray.sort((a, b) => a - b)
+
+            const magnitude = [0,0,0,0,0,0]
+
+           sortedArray.forEach(item => {
+            if(item <= 1) magnitude[0]++;
+            else if(1 < item && item <= 2 ) magnitude[1]++;
+            else if(2 < item && item <= 3 ) magnitude[2]++;
+            else if(3 < item && item<= 4 ) magnitude[3]++;
+            else if(4 < item && item <= 5) magnitude[4]++;
+            else if(5 < item ) magnitude[5]++
+           })
+
+           setData(magnitude)
+
+        }catch(err){
+            console.error(err)
+        }
+    }
+
+    useEffect(()=>getData(),[])
+
+
     return ( 
         <div className="col-9">
-            <h2>Bar chart</h2>
+            <h2 className="mt-4 text-secondary">Bar chart</h2>
+            <hr />
+            {data && <MagnitudeChart data={data}/>}
         </div>
      );
 }
